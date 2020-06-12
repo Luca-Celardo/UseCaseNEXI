a3ebb671b228adb873cab20390793e426a3e4e5e
LucaCelardo <luca.celardo@gmail.com>
2020-06-12 18:45:11 +0200

    Modified project

--- ODSDataMartExtractor/src/main/java/com/example/odsdatamartextractor/OdsdatamartextractorApplication.java
+++ ODSDataMartExtractor/src/main/java/com/example/odsdatamartextractor/OdsdatamartextractorApplication.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class OdsdatamartextractorApplication {
@@ -18,15 +20,20 @@ public class OdsdatamartextractorApplication {
         this.odsDataMartExtractor = odsDataMartExtractor;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(OdsdatamartextractorApplication.class, args);
 
-        List<DataMart> dataMartFiles = odsDataMartExtractor.getAllDataMarts();
-        if(dataMartFiles.size() > 0) {
-            for(DataMart dataMart : dataMartFiles) {
-                odsDataMartExtractor.sendEvent(dataMart, "datamartextractor-topic");
+        Date finishTime = new Date(System.currentTimeMillis() + 300000);
+
+        while(System.currentTimeMillis() < finishTime.getTime()) {
+            List<DataMart> dataMartFiles = odsDataMartExtractor.getAllDataMarts();
+            if(dataMartFiles.size() > 0) {
+                for(DataMart dataMart : dataMartFiles) {
+                    odsDataMartExtractor.sendEvent(dataMart, "datamartextractor-topic");
+                }
+                odsDataMartExtractor.sendEvent(new DataMart(0, "interface-id", "type"), "datamartextractor-source-topic");
             }
-            odsDataMartExtractor.sendEvent(new DataMart(0, "interface-id", "type"), "datamartextractor-source-topic");
+            TimeUnit.SECONDS.sleep(20);
         }
     }
 }
